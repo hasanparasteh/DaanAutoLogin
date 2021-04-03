fbffd5173715ef60e4f9c5dc6455179598cda0dc
Hasan Parasteh <hasanparasteh@gmail.com>
2021-04-03 11:43:32 +0430

    added finder to active classes

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-from os import path, environ
+from os import environ, path
 from time import sleep
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
 from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from win10toast import ToastNotifier
+
 load_dotenv()
 
+toaster = ToastNotifier()
 login_url = "http://iauctb.daan.ir/login-identification-form#login-identification-form"
 username = environ.get("USER")
 password = environ.get("PASS")
@@ -16,7 +19,8 @@ driver = webdriver.Firefox(
 driver.get(login_url)
 
 # Entering user password
-inputs = driver.find_elements_by_css_selector("form#signinform>div.form-group>input")
+inputs = driver.find_elements_by_css_selector(
+    "form#signinform>div.form-group>input")
 inputs[0].send_keys(username)
 inputs[1].send_keys(password)
 sleep(2)
@@ -27,3 +31,15 @@ sleep(2)
 driver.find_element_by_partial_link_text("جلسات من").click()
 
 # Find Exact Active Class
+try:
+    class_login_btn = driver.find_elements_by_partial_link_text("ورود دانشجو")
+
+    if len(class_login_btn) != 0:
+        for i in range(len(class_login_btn)):
+            if not class_login_btn[i].get_attribute('data-target') == ".session_not_reach":
+                toaster.show_toast(
+                    "I'm in class", "Please don't make any sound...Teacher is talking")
+                class_login_btn[i].click()
+except NoSuchElementException:
+    toaster.show_toast("No Active Classes",
+                       "Horayyyy!! There is nothing to do...")
