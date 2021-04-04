f84da6dea0c3fb3b908d5410a8d8917d67b7504d
Hasan Parasteh <hasanparasteh@gmail.com>
2021-04-04 11:53:46 +0430

    add retry to login

--- src/main.py
+++ src/main.py
@@ -1,15 +1,31 @@
 from os import environ, path
 from time import sleep
 
-from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from win10toast import ToastNotifier
 
+from dotenv import load_dotenv
 load_dotenv()
 
+
+def is_session_not_reached(elm):
+    if elm.get_attribute('data-target') == ".session_not_reach":
+        return True
+    else:
+        False
+
+
+def is_teacher_not_in_class(elm):
+    if elm.get_attribute('data-target') == ".teacher-not-entered-classroom":
+        return True
+    else:
+        False
+
+
 toaster = ToastNotifier()
+
 login_url = "http://iauctb.daan.ir/login-identification-form#login-identification-form"
 username = environ.get("USER")
 password = environ.get("PASS")
@@ -23,23 +39,35 @@ inputs = driver.find_elements_by_css_selector(
     "form#signinform>div.form-group>input")
 inputs[0].send_keys(username)
 inputs[1].send_keys(password)
-sleep(2)
+sleep(5)
 driver.find_element_by_css_selector("button.btn-primary").click()
 
-# Go Classes
-sleep(2)
+# Go Classes list
+sleep(5)
 driver.find_element_by_partial_link_text("جلسات من").click()
 
-# Find Exact Active Class
-try:
-    class_login_btn = driver.find_elements_by_partial_link_text("ورود دانشجو")
+# Login into class
+not_in_class = True
+while not_in_class:
+    try:
+        class_login_btn = driver.find_elements_by_partial_link_text(
+            "ورود دانشجو")
 
-    if len(class_login_btn) != 0:
         for i in range(len(class_login_btn)):
-            if not class_login_btn[i].get_attribute('data-target') == ".session_not_reach":
-                toaster.show_toast(
-                    "I'm in class", "Please don't make any sound...Teacher is talking")
+
+            if is_teacher_not_in_class(class_login_btn[i]):
+                toaster.show_toast("Teacher is not here...", "I have to retry logging in...")
+                sleep(60)  # wait for 1 min
+
+            if is_session_not_reached(class_login_btn[i]):
+                toaster.show_toast("Too Soon....! I'll wait for 15 min :)")
+                sleep(900)  # wait for 15 min
+
+            if not is_session_not_reached(class_login_btn[i]) and not is_teacher_not_in_class(class_login_btn[i]):
+                toaster.show_toast("I'm about to login!", "Don't make any sound..I'm going in :)")
                 class_login_btn[i].click()
-except NoSuchElementException:
-    toaster.show_toast("No Active Classes",
-                       "Horayyyy!! There is nothing to do...")
+                not_in_class = False
+
+    except NoSuchElementException:
+        toaster.show_toast("No Active Classes",
+                           "Horayyyy!! There is nothing to do...")
